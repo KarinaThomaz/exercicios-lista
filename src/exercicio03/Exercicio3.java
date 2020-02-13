bc5de0749df4bf7d345a392f2c8a7f02137bb497
Karina Thomaz <karinaapthomaz@gmail.com>
2020-02-13 17:43:18 -0300

    Atualizado

--- src/exercicio03/Exercicio3.java
+++ src/exercicio03/Exercicio3.java
@@ -2,7 +2,9 @@ package exercicio03;
 
 public class Exercicio3 {
     public static void main(String[] args) {
-        Prova prova = new Prova();
+        Prova prova = new Prova(1, 2, 3);
+
+
 
 
     }
--- src/exercicio03/Prova.java
+++ src/exercicio03/Prova.java
@@ -1,24 +1,15 @@
 package exercicio03;
 
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Set;
 
 public class Prova {
-
-    Map<Integer, Integer> mapa=new HashMap<>();
-
-    public Prova(Map<Integer, Integer> mapa) {
-        this.mapa = mapa;
-    }
+    Set<Integer>valor=new HashSet<>();
 
     public void somaTotal(Set<Integer> conjuntoDeInteiros){
-
-        for(Integer chave : mapa.keySet()){
-
-            Integer soma = mapa.get(chave);
+        Integer soma;
+        for(Integer chave : valor){
             soma+=chave;
-            System.out.println(soma);
         }
+        System.out.println(soma);
     }
 }
